5224788f120409886359b208ec799b9176bef7ce
lecheel <lecheel@gmail.com>
2025-08-03 11:28:17 +0800

    Using raw url from github

--- lib/providers/app_state.dart
+++ lib/providers/app_state.dart
@@ -12,7 +12,7 @@ import 'package:path/path.dart' as p;
 class AppState with ChangeNotifier {
   // This is now the default fallback URL.
   static const String DEFAULT_MANIFEST_URL =
-      'https://github.com/lecheel/vocab-san/tree/download_pack/main/manifest.json';
+      'https://raw.githubusercontent.com/lecheel/vocab-san/refs/heads/download_pack/main/manifest.json';
 
   // New variable to hold the current URL.
   String _manifestUrl = DEFAULT_MANIFEST_URL;
